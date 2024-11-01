2cdcc8bdad58a3f4a8de523af8d5cba327568fdb
Selma Gonzalez <117015456+taichousg@users.noreply.github.com>
2024-11-01 16:48:37 +0100

    Update >> add simple popup

--- sketch.js
+++ sketch.js
@@ -10,11 +10,21 @@ function hoverFunction(){
     mouseOver.style.background = "aqua";
 }
 
-// 4. Event Listeners
-document.addEventListener('DOMContentLoaded', function(){
-    const container = document.getElementById('container');
-    const userSelection = document.getElementById('totalSquares');
+function userInput(){
+    let newsquareTotal = prompt("Enter a number of squares per side for the new grid:","16 (Initial Value)");
+    if (newsquareTotal == null || newsquareTotal == ""){
+        squareTotal = 16;
+    } else {
+        squareTotal = newsquareTotal;
+        const container = document.getElementById('container');
+        container.innerHTML = '';
+        initializeSquares();
+    }
+}
 
+function initializeSquares(){
+    const container = document.getElementById('container');
+    const userSelection = document.getElementById('popup');
     if (container){
         // Create the number of squares inside the grid
         for (let i=0; i<squareTotal*squareTotal;i++){
@@ -36,15 +46,22 @@ document.addEventListener('DOMContentLoaded', function(){
     else{
         console.error('Container element not found');
     }
+    
     if (userSelection){
         userSelection.addEventListener('click', function(){
             /* Popup for user to input a number from 16 to 100*/
         });
     }
     else{
-        console.error('Button userSelection element not found');
+        console.error('Button popup element not found');
     }
-    
+    const squareNumber = document.getElementById('squareNumber');
+    squareNumber.textContent = squareTotal;
+}
+
+// 4. Event Listeners
+document.addEventListener('DOMContentLoaded', function(){
+    initializeSquares();
 });
 
 // 5. Main Execution Code
